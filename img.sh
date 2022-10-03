13cc17c1bcf17ce97167f891279e7f7c90333a0e
ishanslab <90043870+ishanslab@users.noreply.github.com>
2022-10-03 10:00:49 +0530

    Update img.sh

--- img.sh
+++ img.sh
@@ -1,3 +1,22 @@
 #!/bin/bash -e
 
 sudo mkdir /home/img-102
+
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install terraform
+
+sudo apt update  && sudo apt install -y curl gnupg apt-transport-https
+
+
+curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+
+
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-debian-bullseye-prod bullseye main" > /etc/apt/sources.list.d/microsoft.list'
+
+
+sudo apt update && sudo apt install -y powershell
+
+
+Install-Module Az.ManagedServiceIdentity -Force
+Install-Module Az.ImageBuilder -Force 
